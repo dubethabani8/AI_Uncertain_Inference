b32cd775df81e2c17a7567e5a5337f8baf7b45cb
dubethabani8 <dubethabani8@gmail.com>
2019-11-05 19:28:44 -0500

    LikelihoodWeightingInferencer working

--- src/bn/inference/LikelihoodWeightingInferencer.java
+++ src/bn/inference/LikelihoodWeightingInferencer.java
@@ -5,33 +5,80 @@ import bn.core.BayesianNetwork;
 import bn.core.Distribution;
 import bn.core.Inferencer;
 import bn.core.RandomVariable;
+import bn.core.Value;
 
 public class LikelihoodWeightingInferencer implements Inferencer{
-
-	public static void main(String[] args) {
-
+	
+	protected class EventAndWeight{
+		Assignment x;
+		double w;
 	}
 
 	@Override
 	public Distribution query(RandomVariable X, Assignment e, BayesianNetwork network) {
-		return likelihoodWeighting(X, e, network, 1000);
+		return likelihoodWeighting(X, e, network, 100000);
 	}
 	
-	public Distribution likelihoodWeighting (RandomVariable X, Assignment e, BayesianNetwork bn, int numSample) {
+	public Distribution likelihoodWeighting (RandomVariable X, Assignment e, BayesianNetwork network, int numSample) {
 		Distribution Q_X = new bn.base.Distribution(X);
 		double[] W = new double[X.getDomain().size()];
+		double sumWeight = 0;
 		
 		for(int j=1; j<=numSample; j++) {
-			Assignment x = WeightedSample(network);
-
+			EventAndWeight xw = weightedSample(network, e);
+			
+			
+			int i = 0;
+			for(Value val: X.getDomain()) {
+				if(val.equals(xw.x.get(X)))
+					W[i] += xw.w;
+				i++;
+			}
+			sumWeight += xw.w;
 		}
 		
-		return null;
+		int i = 0;
+		for(Value val: X.getDomain()) {
+			Q_X.set(val, W[i++]/sumWeight);
+		}
+		
+		Q_X.normalize();
+		return Q_X;
 		
 	}
 	
+	private EventAndWeight weightedSample(BayesianNetwork network, Assignment e) {
+		EventAndWeight xw = new EventAndWeight();
+		Assignment x = new bn.base.Assignment();
+		x.putAll(e);
+		double w = 1;
+		for(RandomVariable Xi: network.getVariablesSortedTopologically()) {
+			if(e.containsKey(Xi)) {
+				x.put(Xi, x.get(Xi));
+				w = w * network.getProbability(Xi, x);
+			}
+			else x.put(Xi, getRandVarAssignment(Xi, x, network));
+		}
+		xw.x = x;
+		xw.w = w;
+		return xw;
+	}
 	
-	
+	private Value getRandVarAssignment(RandomVariable X, Assignment x, BayesianNetwork network) {
+		double rand = Math.random();
+		double sum = 0;
+		Value randomVal = new bn.base.Value(null);
+		for(Value val: X.getDomain()) {
+			x.put(X, val);
+			sum += network.getProbability(X, x);
+			if(rand < sum) {
+				randomVal = val;
+				return randomVal;
+			}
+			x.remove(X);
+		}
+		return randomVal;
+	}
 	
 
 }
